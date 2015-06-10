8f45c131784c8cb4e28bad3a7cdf6c8eec80fb1f
Shareef Dabdoub <shareef@dabdoub.net>
2015-06-10 15:45:35 -0400

    Generalizes table_merge.py to take any 2D table with uniquely identifiable rows

--- bin/table_merge.py
+++ bin/table_merge.py
@@ -10,22 +10,34 @@ from collections import defaultdict, Counter
 import csv
 
 
-def add_data(mg_func, condition):
-    for entry in condition:
-        key = "{};;{};;{};;{}".format(entry['level 1'], entry['level 2'], 
-                                      entry['level 3'], entry['level 4'])
+def add_data(mg_func, table, key_cols):
+    for entry in table:
+        key = ";;".join(["{}" for _ in range(len(key_cols))]).format(*[entry[label] for label in key_cols])
         for ekey in entry:
-            if 'level' not in ekey:
-                mg_func[key]['mgm'+ekey] = entry[ekey]
+            if ekey not in key_cols:
+                mg_func[key][ekey] = entry[ekey]
 
 
-def write_table(mg_func, mgids, out_fp):
+def write_table(mg_func, mgids, key_cols, out_fp):
     with open(out_fp, 'w') as out_f:
-        out_f.write('Level 1\tLevel 2\tLevel 3\tLevel 4\t')
+        out_f.write('\t'.join(key_cols) + '\t')
         out_f.write('\t'.join(mgids) + '\n')
         for func in sorted(mg_func.keys()):
             out_f.write('\t'.join(func.split(';;')) + '\t')
             out_f.write('\t'.join([mg_func[func][mgid] if mgid in mg_func[func] else '0' for mgid in mgids]) + '\n')
+
+
+def parse_key_columns(fp, stop_col):
+    """
+    Given an input file and a (1-indexed) column number,
+    grab all column headers including and preceding 
+    stop_col for use as a unique key in identifying
+    rows.
+    """
+    with open(fp, 'rU') as fh:
+        csvr = csv.reader(fh, delimiter="\t")
+        header = csvr.next()
+        return header[:stop_col]
 
 
 def handle_program_options():
@@ -36,8 +48,13 @@ def handle_program_options():
                                      abundance tables and merge them on the\
                                      list of functions (level 1...level 4),\
                                      e.g. KEGG or mg_func data.")
-    parser.add_argument('abd_tables_fp', nargs="+",
+    parser.add_argument('abd_table_fps', nargs="+",
                         help="Paths to two or more transposed abundance tables.")
+    parser.add_argument('--stop_column', default=1, type=int,
+                        help="The index of the last column before the abundance\
+                              data begins. All columns up to and including this\
+                              one will be combined into a unique key used to\
+                              identify rows for the merge operation.")
     parser.add_argument('-o', '--output_fp', default="merged_table.txt",
                         help="The output file path.")
 
@@ -50,13 +67,15 @@ def main():
     mg_func_abd = defaultdict(lambda : defaultdict(str))
     mg_ids = []
 
-    for fp in args.abd_tables_fp:
+    key_cols = parse_key_columns(args.abd_table_fps[0], args.stop_column)
+
+    for fp in args.abd_table_fps:
         with open(fp, 'rU') as fh:
             abd_table = [line for line in csv.DictReader(fh, delimiter='\t')]
-            add_data(mg_func_abd, abd_table)
-            mg_ids.extend(sorted(["mgm"+entry for entry in abd_table[0] if 'level' not in entry]))
+            add_data(mg_func_abd, abd_table, key_cols)
+            mg_ids.extend(sorted([col_id for col_id in abd_table[0] if col_id not in key_cols]))
 
-    write_table(mg_func_abd, mg_ids, args.output_fp)
+    write_table(mg_func_abd, mg_ids, key_cols, args.output_fp)
 
 
 if __name__ == "__main__":
